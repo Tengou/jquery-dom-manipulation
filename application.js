3370413c35e8d998cc9772ceaa8d19d545985926
Rudy Marle <rudy.marle@orange.fr>
2017-03-06 09:51:29 +0100

    Exo 2-5 done

--- application.js
+++ application.js
@@ -12,19 +12,19 @@ function runChallenges() {
 	
 
   // Ex 2. Fill the content of the email input with your email
-
+	$('#email').val('rudy.marle@orange.fr');
 
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-
+	$('#email-hint').html("<strong>This is my email now</strong>");
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+	$('th').addClass('blue');
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
-  var team_count = 0;  // TODO: replace 0, keep the team_count variable.
+  var team_count = $('.table tbody tr').length;  // TODO: replace 0, keep the team_count variable.
 
 
 
